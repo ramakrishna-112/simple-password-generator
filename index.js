6a1e8cd74aac3b8fe08957cb54bbe2db024a2b9b
ramakrishna-112 <ramakrishna2002dec@gmail.com>
2025-12-02 11:34:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+
+let passwordEl_1 = document.getElementById("password-1");
+let passwordEl_2 = document.getElementById("password-2");
+
+function createPassword(){
+    let password = "";
+    for(let i = 0; i < 15 ; i++){
+        let j = Math.floor(Math.random() * characters.length);
+        password += characters[j];
+    }
+    return password;
+}
+
+function genPasswords(){
+    passwordEl_1.innerHTML = createPassword();
+    passwordEl_2.innerHTML = createPassword();
+}
+
